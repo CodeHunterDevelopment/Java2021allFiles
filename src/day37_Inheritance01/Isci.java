55d5cfccdc2caabb97f98048f7ec3c731b53c845
TahirCesur <delenkar2825@gmail.com>
2021-10-01 11:12:02 +0300

    First Commit

--- src/day37_Inheritance01/Isci.java
+++
@@ -1,8 +0,0 @@
-package day37_Inheritance01;
-
-public class Isci extends Personel {
-	
-	public int saatUcreti = 10 ;
-	public String departman = "Imalathane";
-	public boolean raporluMu = false;	
-}
